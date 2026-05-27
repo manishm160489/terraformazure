f2c3d056e86f0b03b469066570f64a99fd9ad3c9
manish <manishm.164@gmail.com>
2026-05-27 16:08:47 +0530

    added a code for vnet

---
+++ modules/vnet/main.tf
@@ -0,0 +1,6 @@
+resource "azurerm_virtual_network" "newvnet" {
+  name                = "vnet"
+  location            = "West Europe"
+  resource_group_name = "newRG"
+  address_space       = ["10.0.0.0/16"]
+}
